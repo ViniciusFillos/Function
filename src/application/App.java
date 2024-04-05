62430da288e616189523064be93c07dcba67318f
Vinicius Fillos <vinifillos@gmail.com>
2024-04-05 11:08:03 -0300

    Interface Implementation

---
+++ src/application/App.java
@@ -0,0 +1,26 @@
+package application;
+
+import entities.Product;
+import util.UpperCaseName;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        List<Product> list = new ArrayList<>();
+
+        list.add(new Product("Tv",900.00));
+        list.add(new Product("Mouse",50.00));
+        list.add(new Product("Tablet",350.50));
+        list.add(new Product("HD Case",80.90));
+
+        List<String> UpperCase = list.stream().map(new UpperCaseName()).toList();
+
+        UpperCase.forEach(System.out::println);
+    }
+}
