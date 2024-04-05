df27446a0bcba2e5ea6d812921a83ef4e5242e94
Vinicius Fillos <vinifillos@gmail.com>
2024-04-05 11:13:34 -0300

    Declared lambda expression

--- src/application/App.java
+++ src/application/App.java
@@ -6,6 +6,8 @@ import util.UpperCaseName;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.function.Consumer;
+import java.util.function.Function;
 
 public class App {
 
@@ -19,7 +21,9 @@ public class App {
         list.add(new Product("Tablet",350.50));
         list.add(new Product("HD Case",80.90));
 
-        List<String> UpperCase = list.stream().map(Product::UpperCase).toList();
+        Function<Product, String> func = p -> p.getName().toUpperCase();
+
+        List<String> UpperCase = list.stream().map(func).toList();
 
         UpperCase.forEach(System.out::println);
     }
